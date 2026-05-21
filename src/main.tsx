83ea7a0eb45d8d6c1cb993faae8db8bc386c132f
LIPKA <curoae4@gmail.com>
2026-05-21 15:59:16 +0800

    fix: switch to HashRouter for stable deployment on GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import App from './App.tsx'
 import PortfolioWorksPage from './pages/PortfolioWorksPage'
 import CookiePolicyPage from './pages/CookiePolicyPage'
@@ -31,7 +31,7 @@ function Root() {
   }, [])
 
   return (
-    <BrowserRouter basename="/vv-depl/">
+    <HashRouter>
       <>
         <Routes>
           <Route path="/" element={<App />} />
@@ -47,7 +47,7 @@ function Root() {
           </>
         )}
       </>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
